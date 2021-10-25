e94f92b57d9b3b70f1e9aecda9c137dbf8d32e2f
Elyneker Luciani <32239422+ElynekerLuciani@users.noreply.github.com>
2021-10-25 09:56:22 -0300

    criando tabelas do gerenciador de naves

---
+++ Script.sql
@@ -0,0 +1,83 @@
+CREATE TABLE Planetas(
+	IdPlaneta int NOT NULL,
+	Nome varchar(50) NOT NULL,
+	Rotacao float NOT NULL,
+	Orbita float NOT NULL,
+	Diametro float NOT NULL,
+	Clima varchar(50) NOT NULL,
+	Populacao int NOT NULL
+)
+GO
+
+ALTER TABLE Planetas ADD CONSTRAINT PK_Planetas PRIMARY KEY (IdPlaneta);
+GO
+
+CREATE TABLE Naves(
+	IdNave int NOT NULL,
+	Nome varchar(100) NOT NULL,
+	Modelo varchar(150) NOT NULL,
+	Passageiros int NOT NULL,
+	Classe varchar(100) NOT NULL
+)
+GO
+
+ALTER TABLE Naves ADD CONSTRAINT PK_Naves PRIMARY KEY (IdNave);
+GO
+
+CREATE TABLE Pilotos(
+	IdPiloto int NOT NULL,
+	Nome varchar(200) NOT NULL,
+	AnoNascimento varchar(200) NOT NULL,
+	IdPlaneta int NOT NULL
+)
+GO
+
+ALTER TABLE Pilotos ADD CONSTRAINT PK_Pilotos PRIMARY KEY (IdPiloto);
+GO
+
+ALTER TABLE Pilotos ADD CONSTRAINT FK_Pilotos_Planetas FOREIGN KEY(IdPlaneta) REFERENCES Planetas(IdPlaneta);
+GO
+
+CREATE TABLE PilotosNaves(
+	IdPiloto int NOT NULL,
+	IdNave int NOT NULL,
+	FlagAutorizacao bit NOT NULL
+)
+GO
+
+ALTER TABLE PilotosNaves ADD CONSTRAINT PK_PilotosNaves PRIMARY KEY (IdPiloto, IdNave);
+GO
+
+ALTER TABLE PilotosNaves ADD CONSTRAINT FK_PilotosNaves_Pilotos FOREIGN KEY (IdPiloto) REFERENCES Pilotos(IdPiloto);
+GO
+
+ALTER TABLE PilotosNaves ADD CONSTRAINT FK_PilotosNaves_Naves FOREIGN KEY (IdNave) REFERENCES Naves(IdNave);
+GO
+
+ALTER TABLE PilotosNaves ADD CONSTRAINT FK_PilotosNaves_FlagAutorizado DEFAULT (1) FOR FlagAutorizacao;
+GO
+
+CREATE TABLE HistoricoViagens(
+	IdPiloto int NOT NULL,
+	IdNave int NOT NULL,
+	FlagAutorizacao bit NOT NULL,
+	DataSaida datetime NOT NULL,
+	DataChegada datetime NOT NULL
+)
+GO
+
+ALTER TABLE HistoricoViagens ADD CONSTRAINT FK_HistoricoViagens_PilotosNaves FOREIGN KEY (IdPiloto, IdNave)
+REFERENCES PilotosNaves(IdPiloto, IdNave)
+GO
+
+ALTER TABLE HistoricoViagens CHECK CONSTRAINT FK_HistoricoViagens_PilotosNaves;
+
+
+
+
+
+
+
+
+
+
